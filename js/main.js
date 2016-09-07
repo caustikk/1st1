1c213c6c742bb7368be493b670762680c0b09ad1
Konstantin Shavlov <Konstantin Shavlov>
2016-09-07 17:20:20 +0300

    no submit, no scroll-buttons

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+    //accordeon
     $('.accordeon li > h2').click(function(){
         if(!$(this).hasClass('active')){
             $('.accordeon li > h2').removeClass('active').next('.vacancycontent').slideUp();
@@ -7,5 +8,17 @@ $(document).ready(function() {
             } else {
                 $(this).removeClass('active').next('.vacancycontent').slideUp();
             }
+    });
+    // scroll trace
+    var tempScrollTop, currentScrollTop = 0;
+    $(document).scroll(function(){
+        currentScrollTop = $(document).scrollTop();
+        if (tempScrollTop < currentScrollTop){
+            $('header').removeClass('sticktop');
+        }else if(currentScrollTop < 90){
+            $('header').removeClass('sticktop');
+        }else{
+            $('header').addClass('sticktop');}
+        tempScrollTop = currentScrollTop;
     });
 });
