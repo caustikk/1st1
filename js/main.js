734b62f0d7bd129ffeee196536faf12cc4eef54a
Konstantin Shavlov <Konstantin Shavlov>
2016-09-07 11:56:11 +0300

    Accordeon ready

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
 $(document).ready(function() {
-    
+    $('.accordeon li > h2').click(function(){
+        if(!$(this).hasClass('active')){
+            $('.accordeon li > h2').removeClass('active').next('.vacancycontent').slideUp();
+            $(this).addClass('active');
+            $(this).next('.vacancycontent').slideDown(200);
+            } else {
+                $(this).removeClass('active').next('.vacancycontent').slideUp();
+            }
+    });
 });
